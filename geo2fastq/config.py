afd9630f21bceb6ddb8040baad2e88cfd52f1081
Simon van Heeringen <simon.vanheeringen@gmail.com>
2014-01-22 13:51:32 +0100

    XDG compliant configuration file locations

--- geo2fastq/config.py
+++ geo2fastq/config.py
@@ -2,20 +2,25 @@ import os
 import yaml
 from pkg_resources import Requirement, resource_filename
 
+# XDG compliant configuration directories
+HOME = os.path.expanduser("~")
+XDG_CONFIG_HOME = os.environ.get("XDG_CONFIG_HOME", os.path.join(HOME, ".config"))
+XDG_CONFIG_DIRS = [XDG_CONFIG_HOME] + os.environ.get("XDG_CONFIG_DIRS", "/etc/xdg").split(":")
+
 def config():
     pkg = __name__.split(".")[0]
     cfgfile = "{0}.yaml".format(pkg)
     
     global_config_dir = resource_filename(Requirement.parse(pkg), "config")
 
-    paths = [os.curdir, os.path.expanduser("~"), global_config_dir]
-
+    paths = [os.curdir] + [os.path.join(d, pkg) for d in XDG_CONFIG_DIRS] + [global_config_dir]
+    print paths
     for path in paths:
-        for fname in cfgfile, "." + cfgfile:
-            try:
-                with open(os.path.join(path, fname)) as fo:
-                    return yaml.load(fo.read())
-            except IOError:
-              pass
-        
-VERSION = "1.0.1"
+        try:
+            with open(os.path.join(path, cfgfile)) as fo:
+                return yaml.load(fo.read())
+        except IOError:
+            pass
+    raise Exception, "Configuration file {0} not found".format(cfgfile)
+
+VERSION = "1.0.2"
